e014231c233447980ac16beb7415b1dbc1f425f8
Tarunpreet Ubhi <ubhits@outlook.com>
2020-04-16 13:54:57 -0700

    Initial Commit

---
+++ README.md
@@ -0,0 +1,48 @@
+# The TVDB File Renamer
+A quick utility that helps you organize your **Media Library** by scraping **TheTVDB.com** website and renaming your files in a neat and clean fashion. Is **extremely helpful** for **Plex and Kodi** if you know what I mean :wink: :wink:.
+
+Also helps you with missing episodes (example given below). 
+If you like my repo, I really hope you consider :star2:ing it.
+
+## Reduces your effort to organize and correctly name your media files :-
+
+![BeforeAfter](https://ubhits.s3.amazonaws.com/tvdb_renamer/BeforeAfter.png)
+
+The example below is for **Darkwing Duck**, one of my favorite childhood cartoons :)
+
+![Program](https://ubhits.s3.amazonaws.com/tvdb_renamer/Program.png)
+
+And below is the **output** when you run the program
+
+![Terminal](https://ubhits.s3.amazonaws.com/tvdb_renamer/Terminal.png)
+
+- All files that were **found and matched**, are marked in **Green**.
+- All files where the names were **not matched** are marked with **Red**. 
+
+## Diagnosing and Correcting Mismatched Files
+
+Using the output from the program, it becomes very easy to correct the filenames.
+In the example below, if we search **"brainteasers"** from the unmatched file in the **program output**, we find that it's **S03E03**.
+
+![Find](https://ubhits.s3.amazonaws.com/tvdb_renamer/Find.png)
+
+The program was not able to find the file because on tvdb, there is an **"!"** at the end of the filename.
+
+![Diagnose](https://ubhits.s3.amazonaws.com/tvdb_renamer/Diagnose.png)
+
+To correct, I **added the missing "!" to my file** and rerun the program, and **"BOOM goes the DYNAMITE!!!"**
+
+![Corrected](https://ubhits.s3.amazonaws.com/tvdb_renamer/Corrected.png)
+
+## How to Use
+- Find your TV Serial on the TheTVDB.com
+- Copy and paste the URL into the Program
+- Point the files_path to your media file directory
+- Run the Program
+
+I hope this helps you. Please consider :star2:ing my repo if it did. It would encourage me a lot to share more with the community :relaxed:
+
+# HUGE DICLAIMER
+I cannot stress this enough to please backup your files before you run this program. It works, but because we are renaming files on your system, there can be a chance that you lose important data. Please please please backup your data before you even think of running this program. I've tried my best to take care any edge issues, but still don't guarantee 100% stability! I can say with 100% confidence that it's worked for me beautifully! If you find any issues, please feel free to log them here.
+
+![TheTVDB](https://ubhits.s3.amazonaws.com/tvdb_renamer/TheTVDB.png)
---
+++ main.py
@@ -0,0 +1,116 @@
+import requests
+import json
+import os
+from bs4 import BeautifulSoup
+from os import listdir, path
+
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+def scrape_site(url):
+    ep_dict = {}
+    page = requests.get(url)
+    soup = BeautifulSoup(page.content, 'html.parser')
+    results = soup.findAll("h4", {"class": "list-group-item-heading"})
+
+    for result in results:
+        href = result.a['href'].lower()
+        if href.startswith('https://') and ("/official/" in href or  "/specials/" in href):
+            print(f'{bcolors.HEADER}{href}{bcolors.ENDC}')
+
+            season_page = requests.get(result.a['href'])
+            season_soup = BeautifulSoup(season_page.content, 'html.parser')
+            season_results = season_soup.findAll("table", {"class": "table table-condensed table-bordered"})
+            
+            for season in season_results:
+                #print(f'{season}')
+                episodes_table = season.find('tbody')
+                for episode in episodes_table.findAll('tr'):
+                    for td in episode.findAll('td'):
+                        ep_num = td.text.strip()
+                        ep_title = td.find_next_sibling("td").text.strip()
+                        ep_dict[ep_title] = ep_num
+                        #print(f'{ep_num} {ep_title}')
+                        break
+    return ep_dict
+
+
+def write_database(ep_dict, db_path):
+    episodes = json.dumps(ep_dict)
+    f = open(db_path,"w")
+    f.write(episodes)
+    f.close()
+
+
+def print_missing_episodes(ep_dict, ep_dict_backup, test_run):
+    for ep_title in ep_dict.keys():
+
+        test = f"TEST " if test_run else f""
+        missing = test + f"{bcolors.FAIL}MISSING:"
+        found = test + f"{bcolors.OKGREEN + bcolors.BOLD} FOUND :"
+
+        if ep_dict[ep_title] is not None:
+            print(f'{missing} {ep_dict_backup[ep_title]} - {ep_title}{bcolors.ENDC}')
+        else:
+            print(f'{found} {ep_dict_backup[ep_title]} - {ep_title}{bcolors.ENDC}')
+
+
+def rename_files(ep_dict, files_path, test_run):
+    for file in sorted(listdir(files_path)):
+        for ep_title in sorted(ep_dict.keys()):
+            if ' '.join(ep_title.split()).lower() in ' '.join(file.split()).lower():
+                _, file_extension = path.splitext(f'{files_path}/{file}')
+
+                old_filename = f'{files_path}/{file}'
+                new_filename = f'{files_path}/{ep_dict[ep_title]} - {ep_title}{file_extension}'
+
+                if (old_filename == new_filename):
+                    ep_dict[ep_title] = None
+                    continue
+                else:
+                    if ep_dict[ep_title] is not None:
+                        ep_dict[ep_title] = None
+                        if not test_run:
+                            try:
+                                os.rename(old_filename, new_filename)
+                            except:
+                                print(f' ERROR : {old_filename} > {new_filename}')
+
+
+def main():
+
+    URL = 'https://thetvdb.com/series/darkwing-duck'
+    files_path = '/Users/ubhits/Desktop/DD'
+    test_run = False
+
+    db_cache_path = URL[URL.rindex("/") + 1:] + ".json"
+
+    ep_dict = {}
+
+    if path.exists(db_cache_path):
+        ep_dict = json.load(open(db_cache_path))
+    else:
+        ep_dict = scrape_site(URL)
+        write_database(ep_dict, db_cache_path)
+    
+    ep_dict_backup = ep_dict.copy()
+
+    rename_files(ep_dict, files_path, test_run)
+
+    print_missing_episodes(ep_dict, ep_dict_backup, test_run)
+    
+
+if __name__ == "__main__":
+    main()
+
+
+
+
+
